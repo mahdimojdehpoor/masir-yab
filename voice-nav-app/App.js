378a9b8cd424e9e57f539757667bd50f0faf685d
mahdimojdehpoor <mahdimojdehpoor@gmail.com>
2026-08-11 00:22:12 +0330

    Create App.js

---
+++ voice-nav-app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import MapScreen from "./src/screens/MapScreen";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <MapScreen />
+    </>
+  );
+}
