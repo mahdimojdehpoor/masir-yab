8b07fbfe8269da32e9b9cd76c0ffd594f812caa0
mahdimojdehpoor <mahdimojdehpoor@gmail.com>
2026-08-23 03:29:43 +0330

    Wrap MapScreen in ErrorBoundary component

--- voice-nav-app/App.js
+++ voice-nav-app/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import MapScreen from "./src/screens/MapScreen";
+import ErrorBoundary from "./src/components/ErrorBoundary";
 
 export default function App() {
   return (
-    <>
+    <ErrorBoundary>
       <StatusBar style="auto" />
       <MapScreen />
-    </>
+    </ErrorBoundary>
   );
 }
